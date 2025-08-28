a4b0ff71d6a426eb8eaa49a74429c49e931179c6
Fatima Anab <fxtymaanab@gmail.com>
2025-08-28 20:11:55 +0530

    Updated meta tags, homepage image, and preview

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/anab.jpg";
+import myImg from "../../Assets/anab1.png";
 import Tilt from "react-parallax-tilt";
 import {
   AiFillGithub,
